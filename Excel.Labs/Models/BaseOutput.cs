cddb7232e2088b5bfa9467042fbdb79dfcdefaeb
Fatih YILDIZHAN <fatih@momencraft.com>
2019-09-03 16:03:21 +0300

    Create BaseOutput.cs

---
+++ Excel.Labs/Models/BaseOutput.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Doxa.Labs.Excel.Models
+{
+    public class BaseOutput
+    {
+        string Title;
+        string Path;
+        Extension Extension;
+        public BaseOutput(string title, string path, Extension extension)
+        {
+            Title = title;
+            Path = path;
+            Extension = extension;
+        }
+    }
+
+    public enum Extension
+    {
+        [field: Description(".xls")]
+        Xls,
+        [field: Description(".xls")]
+        Xlsx
+    }
+}
