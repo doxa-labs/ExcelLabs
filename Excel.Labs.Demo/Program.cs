344de2e927b07b469c66367299105b14b7e802dd
Fatih YILDIZHAN <fatih@momencraft.com>
2019-09-19 20:31:40 +0300

    Update Demo

--- Excel.Labs.Demo/Program.cs
+++ Excel.Labs.Demo/Program.cs
@@ -16,17 +16,20 @@ namespace Excel.Labs.Demo
             // Console.WriteLine(bo.Extension);
             Console.WriteLine(excel.FilePath);
 
-            
+            // create a cell list
             List<LabsCell> cells = new List<LabsCell>();
-            /*
+
+            // define row and column indexes then add your data
+            cells.Add(new LabsCell(10, 20, "Your Value"));
+
+            // add some data to cell list
             for (int i = 1; i < 20; i++)
             {
                 cells.Add(new LabsCell(i, i, i));
             }
-            */
 
-            // save excel file
-            excel.Save(null);
+            // call save function with the cell list
+            excel.Save(cells);
 
             Console.ReadLine();
         }
