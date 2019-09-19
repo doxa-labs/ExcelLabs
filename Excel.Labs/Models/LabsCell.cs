d80fb54e110cd340d15ca98f811470554a7cd3fa
Fatih YILDIZHAN <fatih@momencraft.com>
2019-09-19 18:37:52 +0300

    Add Contstructor

--- Excel.Labs/Models/LabsCell.cs
+++ Excel.Labs/Models/LabsCell.cs
@@ -5,5 +5,12 @@
         public int RowIndex { get; set; }
         public int ColumnIndex { get; set; }
         public dynamic Value { get; set; }
+
+        public LabsCell(int rowIndex, int columnIndex, dynamic value)
+        {
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+            Value = value;
+        }
     }
 }
